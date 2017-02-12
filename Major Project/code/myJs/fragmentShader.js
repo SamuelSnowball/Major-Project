60e70b2cf18a575fc4368bda1afd769e22d5dfec
SamSnowball <sam.snowball@hotmail.co.uk>
2017-02-12 23:14:34 +0000

    See 12th feb changelog

--- Major Project/code/myJs/fragmentShader.js
+++ Major Project/code/myJs/fragmentShader.js
@@ -2,13 +2,18 @@
 Fragment
 want shader to use high precision on floats
 have to use medium for some browsers
+
+No longer assigning color values to fragments color
+The fragment color is computed by getting the texel,
+(pixel within texture) that sampler says maps to fragments position
 */
 var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, [
 	'precision highp float;',
-	'varying vec3 vertexColor;',
+	'varying highp vec2 vTextureCoord;',
+	'uniform sampler2D uSampler;',
 	'void main(){',
-		'gl_FragColor = vec4(vertexColor, 1);',
+		'gl_FragColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));',
 	'}'
 ].join('\n'));
 gl.compileShader(fragmentShader);
