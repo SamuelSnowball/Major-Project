21ac124f391d11ccc99a9efc45dce6c864565a89
SamSnowball <sam.snowball@hotmail.co.uk>
2017-04-23 18:54:32 +0100

    updated docs

---
+++ Major Project/documentation/docs/out/api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Camera",
+        "CollisionTester",
+        "MainProgram",
+        "MarsScene",
+        "Minimap",
+        "MyGUI",
+        "ParticleSystem",
+        "PointerLockControls",
+        "RockGenerator",
+        "Skybox",
+        "SkyboxProgram",
+        "Sound",
+        "Terrain",
+        "TesterClass",
+        "Texture",
+        "Utility",
+        "WaterFramebuffers",
+        "WaterProgram",
+        "WaterSystem"
+    ],
+    "modules": [
+        "Engine"
+    ],
+    "allModules": [
+        {
+            "displayName": "Engine",
+            "name": "Engine",
+            "description": "Fragment shader"
+        }
+    ],
+    "elements": []
+} };
+});
